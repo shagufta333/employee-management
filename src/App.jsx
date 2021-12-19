94ddddfd792e55672aac5db839459bddb4541da2
shagufta <shagufta.batool8@gmail.com>
2021-12-19 23:24:44 +0100

    passed the first test

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,11 @@
-import React from "react";
+import React, { Component } from "react/cjs/react.development";
 
-const App = () => {
-
-  return (
-    <h1>
-      Hello World
-    </h1>
-  );
-};
-
-
-
+class App extends Component {
+  render() {
+    return <div>
+      <h1 id='header'>Employee List</h1>
+    </div>;
+  }
+}
 
 export default App;
