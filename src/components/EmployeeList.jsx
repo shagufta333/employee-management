0b323fed9e7c9f41f7e3210cef9bb837c586bb3b
shagufta <shagufta.batool8@gmail.com>
2021-12-20 02:35:46 +0100

    refactored employeeList component to fetch data from an API using httpClient Axios library to display employee list rather then storing data in component state

--- src/components/EmployeeList.jsx
+++ src/components/EmployeeList.jsx
@@ -1,45 +1,17 @@
 import React, { Component } from "react";
-
+import axios from "axios";
 class EmployeeList extends Component {
   state = {
-    employees: [
-      {
-        id: 1,
-        first_name: "George",
-        last_name: "Bluth",
-        avatar: "https://reqres.in/img/faces/1-image.jpg",
-      },
-      {
-        id: 2,
-        first_name: "Janet",
-        last_name: "Weaver",
-        avatar: "https://reqres.in/img/faces/2-image.jpg",
-      },
-      {
-        id: 3,
-        first_name: "Emma",
-        last_name: "Wong",
-        avatar: "https://reqres.in/img/faces/3-image.jpg",
-      },
-      {
-        id: 4,
-        first_name: "Eve",
-        last_name: "Holt",
-        avatar: "https://reqres.in/img/faces/4-image.jpg",
-      },
-      {
-        id: 5,
-        first_name: "Charles",
-        last_name: "Morris",
-        avatar: "https://reqres.in/img/faces/5-image.jpg",
-      },
-      {
-        id: 6,
-        first_name: "Tracey",
-        last_name: "Ramos",
-        avatar: "https://reqres.in/img/faces/6-image.jpg",
-      },
-    ],
+    employees: [],
+  };
+
+  componentDidMount() {
+    this.getEmployees();
+  }
+
+  getEmployees = async () => {
+    let employeeData = await axios.get("https://reqres.in/api/users");
+    this.setState({ employees: employeeData.data.data });
   };
   render() {
     let employeeList = this.state.employees.map((employee) => {
