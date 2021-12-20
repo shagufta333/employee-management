fcd26748901d55539f8f80d811ab3487641b08cf
shagufta <shagufta.batool8@gmail.com>
2021-12-20 01:10:53 +0100

    added test for the employee list to have 6 employees

--- cypress/integration/1-getting-started/displayEmployeeList.feature.js
+++ cypress/integration/1-getting-started/displayEmployeeList.feature.js
@@ -1,8 +1,22 @@
-describe('Display list of employees', () => {
-    beforeEach(() => {
-      cy.visit('/')
+describe("Display list of employees", () => {
+  beforeEach(() => {
+    cy.visit("/");
+  });
+  it("display a header", () => {
+    cy.get("#header").should("contain", "Employee List");
+  });
+
+  it("displays a list with 6 items", () => {
+    cy.get("#employee-list").within(() => {
+      cy.get(".employee-item").should("have.length", 6);
     });
-    it('display a header', () => {
-      cy.get('#header').should('contain', 'Employee List')
+  });
+  it("the list items display the expected content", () => {
+    cy.get("#employee-list").within(() => {
+      cy.get(".employee-item")
+        .first()
+        .find(".name")
+        .should("contain", "George Bluth");
     });
+  });
 });
